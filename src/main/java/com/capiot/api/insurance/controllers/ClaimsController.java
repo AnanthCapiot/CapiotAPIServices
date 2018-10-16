7b4d45848d6c308198aa442e2c8aa95bf11af121
Ananth Krishnan <ananth@capiot.com>
2018-10-16 10:48:34 +0530

    added gitignore file

--- src/main/java/com/capiot/api/insurance/controllers/ClaimsController.java
+++ src/main/java/com/capiot/api/insurance/controllers/ClaimsController.java
@@ -1,7 +1,7 @@
 package com.capiot.api.insurance.controllers;
 
 import com.capiot.api.insurance.model.Claim;
-import com.capiot.api.travel.model.Booking;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +14,11 @@ public class ClaimsController {
     public List<Claim> getAllClaims() throws Exception {
 //        List<Claim> allBookings = bookingService.getAllBookings();
         return null;
+    }
+
+    @RequestMapping(value = "/bookings", method = RequestMethod.POST)
+    public Claim createBooking( @RequestBody Claim newClaim )	{
+        // create new Booking, return Booking with generated ID
+        return newClaim;
     }
 }
